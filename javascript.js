7b594030faffe058b4b209d90d572882f10eae3a
Luke Coty <Lukecoty4@gmail.com>
2022-06-02 12:23:28 -0400

    added function playGame() to start the game.

--- javascript.js
+++ javascript.js
@@ -1,22 +1,36 @@
+let playerSelection = prompt("Enter Rock, Paper, or Scissor: ");
+let computerSelection;
+
 function computerPlay() {
     let randomNumber = Math.floor(Math.random()*3) + 1;
     console.log(randomNumber);
 
     if (randomNumber === 1){
-        console.log("Rock");
+        computerSelection = "Rock";
     } else if (randomNumber === 2) {
-        console.log("Paper");
+        computerSelection = "Paper";
     } else if (randomNumber === 3){
-        console.log("Scissor");
+        computerSelection = "Scissor";
+    }
+}
+computerPlay()
+function playGame(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection){
+        console.log("DRAW!");
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        console.log("You lose! Paper beats Rock");
+    } else if (playerSelection === "Rock" && computerSelection === "Scissor") {
+        console.log("You win! Rock beats Scissor");
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        console.log("You win! Paper beats Rock");
+    } else if (playerSelection === "Paper" && computerSelection === "Scissor") {
+        console.log("You lose! Scissor beats Paper");
+    } else if (playerSelection === "Scissor" && computerSelection === "Rock") {
+        console.log("You lose! Rock beats Scissor");
+    } else if (playerSelection === "Scissor" && computerSelection === "Paper") {
+        console.log("You win! Scissor beats Paper");
     }
 
 }
 
-computerPlay();
-computerPlay();
-computerPlay();
-computerPlay();
-computerPlay();
-computerPlay();
-computerPlay();
-computerPlay();
+playGame(playerSelection, computerSelection);
