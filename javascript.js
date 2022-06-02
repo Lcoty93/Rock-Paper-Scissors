4b68ac28c309a891634966aa379ecf5bf1f35a91
Luke Coty <Lukecoty4@gmail.com>
2022-06-02 13:14:26 -0400

    Finished console.log game

--- javascript.js
+++ javascript.js
@@ -1,36 +1,72 @@
-let playerSelection = prompt("Enter Rock, Paper, or Scissor: ");
+let promptPlayer;
+let playerSelection;
 let computerSelection;
+let robotWins = 0;
+let playerWins = 0;
 
 function computerPlay() {
     let randomNumber = Math.floor(Math.random()*3) + 1;
     console.log(randomNumber);
 
     if (randomNumber === 1){
-        computerSelection = "Rock";
+        return "ROCK";
     } else if (randomNumber === 2) {
-        computerSelection = "Paper";
+        return "PAPER";
     } else if (randomNumber === 3){
-        computerSelection = "Scissor";
+        return "SCISSOR";
     }
 }
-computerPlay()
-function playGame(playerSelection, computerSelection) {
+
+
+
+function playRound(playerSelection, computerSelection) {
+
     if (playerSelection === computerSelection){
-        console.log("DRAW!");
-    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        console.log("You lose! Paper beats Rock");
-    } else if (playerSelection === "Rock" && computerSelection === "Scissor") {
-        console.log("You win! Rock beats Scissor");
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        console.log("You win! Paper beats Rock");
-    } else if (playerSelection === "Paper" && computerSelection === "Scissor") {
-        console.log("You lose! Scissor beats Paper");
-    } else if (playerSelection === "Scissor" && computerSelection === "Rock") {
-        console.log("You lose! Rock beats Scissor");
-    } else if (playerSelection === "Scissor" && computerSelection === "Paper") {
-        console.log("You win! Scissor beats Paper");
+        return "DRAW!";
+    } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+        robotWins += 1;
+        return "You lose! Paper beats Rock";
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSOR") {
+        playerWins += 1;
+        return "You win! Rock beats Scissor";
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        playerWins += 1;
+        return "You win! Paper beats Rock";
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSOR") {
+        robotWins += 1;
+        return "You lose! Scissor beats Paper";
+    } else if (playerSelection === "SCISSOR" && computerSelection === "ROCK") {
+        robotWins += 1;
+        return "You lose! Rock beats Scissor";
+    } else if (playerSelection === "SCISSOR" && computerSelection === "PAPER") {
+        playerWins += 1;
+        return "You win! Scissor beats Paper";
     }
 
 }
 
-playGame(playerSelection, computerSelection);
+function game() {
+
+    for(let i = 0; i < 5; i++){
+        promptPlayer = prompt("Enter Rock, Paper, or Scissor: ");
+        playerSelection = promptPlayer.toUpperCase();
+        computerSelection = computerPlay();
+
+        
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("Robot wins: " + robotWins);
+        console.log("Your wins: " + playerWins);
+        
+    }
+
+    if(playerWins > robotWins){
+        console.log("Out of 5 games,")
+        console.log("You won the game!");
+    } else {
+        console.log("Out of 5 games,")
+        console.log("Robot won the game!");
+    }
+
+}
+
+game();
